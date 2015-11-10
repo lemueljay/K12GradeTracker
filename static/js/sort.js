24aa58a889001167f96a0e31ac57518107ccad58
Lemuel Jay Vallinas <lemueljayvallinas@gmail.com>
2015-11-11 07:26:51 +0800

    UPDATE: Sortable Tables

---
+++ static/js/sort.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $('#myClassTable').tablesorter();
+    $('#myStudentsTable').tablesorter();
+    $('#myAssessmentsTable').tablesorter();
+    $('#myAssessmentTable').tablesorter();
+    $('#myGradesTable').tablesorter();
+});
