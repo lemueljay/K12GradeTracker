7c05e828d14a1a4cce25f8c430a579ece312fa9c
Lemuel Jay Vallinas <lemueljayvallinas@gmail.com>
2015-11-26 19:05:22 +0800

    Isotope Classes Template

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -107,6 +107,7 @@ function updateTime() {
             $('#ampm').text(m);
         }
     }
+
 }
 
 function initBars() {
@@ -114,7 +115,65 @@ function initBars() {
     $('#mainbar').show();
 }
 
+
+
+
 $(document).ready(function() {
+
+    // external js: isotope.pkgd.js
+
+
+  // init Isotope
+  var $container = $('.isotope-classes').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+      name: '.name',
+        section: '.section',
+        type: '.type'
+    }
+  });
+
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
+
+  // bind sort button click
+  $('#sorts').on( 'click', 'button', function() {
+    var sortByValue = $(this).attr('data-sort-by');
+    $container.isotope({ sortBy: sortByValue });
+  });
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+
+
+
+
     updateTime();
     initBars();
     alertify.warning('Welcome '+ $('.contentbar-firstname').text() + ' !');
